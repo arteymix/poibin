3705071589167aa885b3f86d130a57e5b4b96b75
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2019-06-28 09:34:19 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='poibin',
+      author='Mika J. Straka',
+      description='Poisson Binomial Probability Distribution for Python',
+      url='https://github.com/tsakim/poibin',
+      licence='MIT',
+      py_modules=['poibin'])
